72ac8d01e0fe7fc539305bf342974a5066c131f8
FaiqaTalib <95124315+FaiqaTalib@users.noreply.github.com>
2026-01-04 23:02:16 -0800

    Add files via upload

---
+++ products.js
@@ -0,0 +1,27 @@
+ async function datafetch() {
+     let response = await fetch('https://dummyjson.com/products', { method: 'get' });
+     let data = await response.json();
+     let record = data.products
+     console.log(record[0].title);
+
+
+
+     document.getElementById('prodrow');
+     prodrow.innerHTML = '';
+     for (i = 0; i < record.length; i++) {
+         prodrow.innerHTML += `
+         <div class="card" style="width: 18rem;">
+  <img src="${record[i].thumbnail}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${record[i].title}</h5>
+    <p class="card-text">${record[i].description}</p>
+    <h6>$ ${record[i].price}</h6>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+         `
+     }
+
+ }
+
+ datafetch()
